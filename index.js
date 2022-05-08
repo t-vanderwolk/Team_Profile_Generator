423a1125483e6a7d8debe3f24cc2d408414ab4c9
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-08 14:38:10 -0700

    inital commit with filestructure and packages

---
+++ index.js
@@ -0,0 +1,38 @@
+const Manager = require("./js/Manager.js");
+const Engineer = require("./js/Engineer.js");
+const Intern = require("./js/Intern.js");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const generateTeam = require("./src/template.js")
+
+teamArray = [];
+
+
+
+function runApp () {
+
+  function createTeam () {
+    inquirer.prompt([{
+      type: "list",
+      message: "What type of employee would you like to add to your team?",
+      name: "addEmployeePrompt",
+      choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+    }]).then(function (userInput) {
+      switch(userInput.addEmployeePrompt) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+
+        default:
+          htmlBuilder();
+    }});
+}}

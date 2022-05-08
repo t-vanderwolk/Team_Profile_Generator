98724261c3db123f060d572d5ada8a38e3c0f156
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-08 14:56:41 -0700

    made individuale js functions and restructed files

--- index.js
+++ index.js
@@ -36,3 +36,38 @@ function runApp () {
           htmlBuilder();
     }});
 }}
+
+function addManager() {
+    inquirer.prompt ([
+      
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?"
+      },
+  
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's employee ID number?"
+      },
+  
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?"
+      },
+  
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the manager's office number?"
+      }
+  
+    ]).then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      teamArray.push(manager);
+      createTeam();
+    });
+  
+  }
